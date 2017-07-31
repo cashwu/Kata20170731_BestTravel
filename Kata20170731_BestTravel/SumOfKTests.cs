a1dcfb553894456a4b74a2e09d44d3d88d8e560b
cash <cash@cashwu.com>
2017-07-31 16:39:50 +0800

    distance 30, visit 2, ls 20, 15 test case

--- Kata20170731_BestTravel/SumOfKTests.cs
+++ Kata20170731_BestTravel/SumOfKTests.cs
@@ -8,9 +8,9 @@ namespace Kata20170731_BestTravel
     public class SumOfKTests
     {
         [TestMethod]
-        public void input_distance_10_visit_2_ls_50_should_return_null()
+        public void input_distance_10_visit_1_ls_50_should_return_null()
         {
-            AssertShooseBestSum(10, 2, new List<int> { 50 }, null);
+            AssertShooseBestSum(10, 1, new List<int> { 50 }, null);
         }
 
         [TestMethod]
@@ -25,6 +25,18 @@ namespace Kata20170731_BestTravel
             AssertShooseBestSum(20, 1, new List<int> { 20, 15 }, 20);
         }
 
+        [TestMethod]
+        public void input_distance_20_visit_2_ls_20_15_should_return_20()
+        {
+            AssertShooseBestSum(20, 1, new List<int> { 20, 15 }, 20);
+        }
+
+        [TestMethod]
+        public void input_distance_30_visit_2_ls_20_15_should_return_null()
+        {
+            AssertShooseBestSum(30, 2, new List<int> { 20, 15 }, null);
+        }
+
         private static void AssertShooseBestSum(int maxDistance, int visitCount, List<int> listOfDistance, int? expected)
         {
             var sumOfk = new SumOfK();
@@ -37,6 +49,12 @@ namespace Kata20170731_BestTravel
     {
         public int? chooseBestSum(int maxDistance, int visitCount, List<int> listOfDistance)
         {
+            if (visitCount == listOfDistance.Count
+                && listOfDistance.Sum() > maxDistance)
+            {
+                return null;
+            }
+
             int? choose = listOfDistance.FirstOrDefault(a => maxDistance >= a);
             return choose == 0 ? default(int?) : choose;
         }

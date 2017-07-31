2b5fe39b1b98be1ec574f12c1fff8db084be7aff
cash <cash@cashwu.com>
2017-07-31 12:28:34 +0800

    distance 10, visit 2, ls 50 test case

---
+++ Kata20170731_BestTravel/SumOfKTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170731_BestTravel
+{
+    [TestClass]
+    public class SumOfKTests
+    {
+        [TestMethod]
+        public void input_distance_10_visit_2_ls_50_should_return_null()
+        {
+            AssertShooseBestSum(10, 2, new List<int>(10), null);
+        }
+
+        private static void AssertShooseBestSum(int t, int k, List<int> ls, int? expected)
+        {
+            var sumOfk = new SumOfK();
+            var actual = sumOfk.chooseBestSum(t, k, ls);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class SumOfK
+    {
+        public int? chooseBestSum(int t, int k, List<int> ls)
+        {
+            return null;
+        }
+    }
+}
